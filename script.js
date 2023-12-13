7bf2f3f9e90dcc65649e4412edf2742ec8e28708
Patrick Moran <morancc@outlook.com>
2023-12-13 13:34:13 +1100

    Determinant of 4x4 matrix complete

--- script.js
+++ script.js
@@ -4,11 +4,6 @@
 
 //Compute determinant function.
 const computeDeterminant = array => {
-    //Validate user input.
-    if (array.length != array[0].length){
-        console.log("Error - not a square matrix.");
-    }
-
     let determinant;
 
     //Compute determinant for a 2x2 matrix.
@@ -32,20 +27,55 @@ const computeDeterminant = array => {
         }
         return localDeterminant;
     }
+
+    //Compute determinant for a 4x4 matrix.
+    const nEqualsFour = fourArray => {
+        let localDeterminant = 0;
+        let sign = 1;
+        let j = 3;
+        let cofactorsArray = fourArray.splice(0,1);
+        for (let i = 0; i < fourArray.length + 1; i++){
+            let mutatedArray = [(fourArray[0].toSpliced(j,1)),(fourArray[1].toSpliced(j,1)),(fourArray[2].toSpliced(j,1))];
+            let intDeterminant = nEqualsThree(mutatedArray);
+            sign = sign * -1;
+            localDeterminant += (sign * cofactorsArray[0][j] * intDeterminant);
+            j--;
+        }
+        return localDeterminant;
+    }
+
     if (array.length == 2){
-        determinant = nEqualsTwo(array);
+        //Validate user input.
+        if (array.length != array[0].length || array.length != array[1].length){
+            console.log("Error - not a square matrix.");
+        } else {
+            determinant = nEqualsTwo(array);
+        }
     } else if (array.length == 3){
-        determinant = nEqualsThree(array);
+        //Validate user input.
+        if (array.length != array[0].length || array.length != array[1].length || array.length != array[2].length){
+            console.log("Error - not a square matrix.");
+        } else {
+            determinant = nEqualsThree(array);
+        }
+    } else if (array.length == 4){
+        //Validate user input.
+        if (array.length != array[0].length || array.length != array[1].length || array.length != array[2].length || array.length != array[3].length){
+            console.log("Error - not a square matrix.");
+        } else {
+            determinant = nEqualsFour(array);
+        }
     }
 
     return determinant;
 }
 
 //TEST:
-let testArray = [[1,2,5],
-                [4,5,3],
-                [7,2,8]];
-//The above test structure is the same as [[[00],[01]],[[10],[11]]].
+let testArray = [[1,2,5,6],
+                [4,5,3,2],
+                [7,2,8,0],
+                [5,1,2,7]];
+//The above test structure is the same as [[[00],[01]],[[10],[11]]...].
 //That is, array[n] contains the rows, and array[n][m] is the data for the row, with [m] changing as the column changes.
 
 let test = computeDeterminant(testArray);
